da5c41221c30fe5a6500bc0b1485f4da0aa904d6
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-03-15 10:18:30 +0900

    Initial Commit

---
+++ BaronClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaronClient
+//
+//  Created by tanaka.naoto on 2019/03/15.
+//  Copyright © 2019年 tanaka.naoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
