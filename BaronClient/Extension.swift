97cdb77777a9fe2d4889f00fc8833783ce439762
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-03-29 16:30:04 +0900

    タップした位置に配置されるモデルを、箱から3D猫に変更
    refs #53488

--- BaronClient/Extension.swift
+++ BaronClient/Extension.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019年 tanaka.naoto. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SceneKit
+
+extension SCNNode {
+    
+    /**
+     * .scnファイルをSCNNodeで読み込む
+     * ex.) let node : SCNNode =  SCNNode(named:"art.scnassets/cat.scn")
+     */
+    convenience init(named name: String) {
+        self.init()
+        
+        guard let scene = SCNScene(named: name) else {
+            return
+        }
+        
+        for childNode in scene.rootNode.childNodes {
+            addChildNode(childNode)
+        }
+    }
+    
+}
