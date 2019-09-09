b2a0ec92454d333971f21be82a0977fde2ec6d34
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-09-09 16:32:22 +0900

    Endpoint.swiftを改めて作成

---
+++ BaronClient/Network/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  BaronClient
+//
+//  Created by tanaka.naoto on 2019/03/29.
+//  Copyright © 2019年 tanaka.naoto. All rights reserved.
+//
+
+enum Endpoint : String {
+    case root = "XXXXXXXXX"
+    case getCat = "/getCat"
+}
+
